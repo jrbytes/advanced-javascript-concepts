75171fa87c62155ae76655369ad14804a89d2954
JrBytes <itamazor@gmail.com>
2020-04-04 15:57:59 -0300

    Módulo Types in JS, JS Types e Array isArray

---
+++ 2-types-in-javascript/01-javascript-types.js
@@ -0,0 +1,15 @@
+// Existem sete tipos
+
+// Tipos primitivos - apresentam apenas um valor
+typeof 5
+typeof true
+typeof 'like'
+typeof undefined
+typeof null // retorna um objeto, se resolver pode corromper vários sistemas
+typeof Symbol('Just me')
+
+// Tipos não primitivos
+typeof {}
+// Matrizes e funções são objetos
+typeof [] // retorna um objeto
+typeof function(){} // as funções são objetos
---
+++ 2-types-in-javascript/02-array-isarray.js
@@ -0,0 +1,10 @@
+var array = ['1','2','3']
+
+// var array = {
+//   0: '1',
+//   1: '2',
+//   2: '3'
+// }
+// false
+
+console.log(Array.isArray(array))
