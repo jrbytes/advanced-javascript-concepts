902cd1414133f4d56474e26a9313335c4c3f63d0
Jr Bytes <itamazor@gmail.com>
2020-06-09 15:23:22 -0300

    Modulo Closures and Prototypal Inheritance, Prototypal Inheritance.

---
+++ 3-closures-and-prototypal-inheritance/18-prototypal-inheritance.js
@@ -0,0 +1,16 @@
+Date.prototype.lastYear = function() {
+  return this.getFullYear() - 1
+}
+new Date('1900-10-10').lastYear()
+
+Array.prototype.map = function() {
+  let arr = []
+  for (let i = 0; i < this.length; i++) {
+    arr.push((this[i] + '🗺'))
+  }
+  return arr
+}
+console.log([1, 2, 3].map())
+
+// Esse tipo de alteração é perigosa, porque alterar o prototype,
+// fará essa alteração se propagar pelo app
