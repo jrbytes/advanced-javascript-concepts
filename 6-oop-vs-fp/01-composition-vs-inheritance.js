caeb82c601d64f2d3128fba2bf32402e7613dacc
Jr Bytes <itamazor@gmail.com>
2020-06-14 23:56:54 -0300

    Modulo OOP vs FP, Composition vs Inheritance, OOP vs FP.

---
+++ 6-oop-vs-fp/01-composition-vs-inheritance.js
@@ -0,0 +1,6 @@
+// Inheritance - What it is  :: O que são
+// Tight coupling - Fragile base class problem - Hierarchy
+// As coisas podem mudar no futuro, e por causa do acoplamento rígido,
+// qualquer mudança em uma classe pode alterar um comportamento em outra.
+
+// Composition - What it has :: Quais são as habilidades
---
+++ 6-oop-vs-fp/02-oop-vs-fp.md
@@ -0,0 +1,11 @@
+fp
+  many operations on fixed data
+  stateless
+  pure
+  declarative
+
+oop
+  few operations on common data
+  stateful
+  side effect
+  imperative
